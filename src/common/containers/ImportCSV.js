3cdfe0297cd583a4618bc53c6ddcb5c107ceab05
Chris Martin <chris@crunchycloud.io>
2019-03-27 13:30:50 +1000

    prevent files that are too large, remove .env from repo

--- src/common/containers/ImportCSV.js
+++ src/common/containers/ImportCSV.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Button, Icon } from 'semantic-ui-react'
+import { Button, Icon, Confirm, Message } from 'semantic-ui-react'
 import * as Papa from 'papaparse';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import db from '../../util/db';
 import Dropzone from 'react-dropzone';
+
+import '../styles/ImportCSV.css';
 
 import {
   fileChosen as _fileChosen,
@@ -17,53 +19,92 @@ class ImportCSV extends Component {
   constructor(props){
     super(props);
     this.onFileChosen = this.onFileChosen.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.state = {
+      fileTooBig: false
+    }
+  }
+
+  importFile(file) {
+    const { fileChosen, importComplete, columnsUpdated } = this.props;    
+
+    fileChosen(file);
+    localStorage.setItem(process.env.REACT_APP_FILE_NAME_KEY, file.name)
+    db[process.env.REACT_APP_DB_TABLE_NAME].clear()
+    .then(() => {
+      Papa.parse(file, {
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        complete: function(results) {
+          console.log(results)
+          const columns = results.meta.fields;
+          columnsUpdated(columns);
+          localStorage.setItem(process.env.REACT_APP_COLUMN_NAMES_KEY, columns);
+          db[process.env.REACT_APP_DB_TABLE_NAME].bulkAdd(results.data)
+          .then(() => {
+            importComplete(results.data.length);
+          })
+        }
+      });
+    })
   }
 
   onFileChosen(acceptedFiles) {
-    const { fileChosen, importComplete, columnsUpdated } = this.props;
+
     if(acceptedFiles.length) {
-      let [file] = acceptedFiles
-      let sampleRow = null;
-      
-      fileChosen(file);
-      localStorage.setItem(process.env.REACT_APP_FILE_NAME_KEY, file.name)
-      db[process.env.REACT_APP_DB_TABLE_NAME].clear()
-      .then(() => {
-        Papa.parse(file, {
-          header: true,
-          dynamicTyping: true,
-          skipEmptyLines: true,
-          complete: function(results) {
-            sampleRow = results.data[0];
-            const columns = Object.keys(sampleRow);
-            columnsUpdated(columns);
-            localStorage.setItem(process.env.REACT_APP_COLUMN_NAMES_KEY, columns);
-            db[process.env.REACT_APP_DB_TABLE_NAME].bulkAdd(results.data)
-            .then(() => {
-              importComplete(results.data.length);
-            })
-          }
-        });
-      })
+      const [file] = acceptedFiles;
+      if(file.size <= process.env.REACT_APP_MAX_FILE_SIZE){
+        this.importFile(file);
+      } else {
+        this.setState({
+          fileTooBig: true
+        })
+      }
     }
+  }
+
+  closeModal() {
+    this.setState({
+      fileTooBig: false
+    })
   }
 
   render() {
     const { data } = this.props;
+    const { fileTooBig } = this.state;
     return (
-    <Dropzone onDrop={this.onFileChosen}>
-        {({getRootProps, getInputProps}) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <Button loading={data.importing} disabled={data.importing} icon labelPosition='left' color="teal" size="small">
-                <Icon name={data.importing ? 'circle notched' : 'upload'} />
-                Import CSV
-              </Button>
+      <React.Fragment>
+        <Dropzone onDrop={this.onFileChosen}>
+          {({getRootProps, getInputProps}) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <Button loading={data.importing} disabled={data.importing} icon labelPosition='left' color="teal" size="small">
+                  <Icon name={data.importing ? 'circle notched' : 'upload'} />
+                  Import CSV
+                </Button>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        <Confirm 
+          open={fileTooBig} 
+          onCancel={this.closeModal} 
+          onConfirm={this.closeModal}
+          header="File too large"
+          content={(
+            <div className="file-too-big-content">
+              <Message color="orange">
+                <Message.Header>Sorry about that</Message.Header>
+                If we try to process this file, 
+                your computer will curl up in the fetal position and cry.<br/>
+                We are working on supporting larger files.
+              </Message>
             </div>
-          </section>
-        )}
-      </Dropzone>
+          )}
+        />
+      </React.Fragment>
     )
   }
 }
