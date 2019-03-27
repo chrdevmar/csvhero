74193d45a9e40168eb759d5d984a0da1f926d713
Chris Martin <chris@crunchycloud.io>
2019-03-28 00:04:54 +1000

    use filter as default view when /app is matched on wildcard

--- src/common/components/ActionPanel.js
+++ src/common/components/ActionPanel.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import '../styles/ActionPanel.css';
 
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
 import NavItems from './NavItems';
 import Filter from '../../filter/containers/Filter';
@@ -15,11 +15,13 @@ class ActionPanel extends Component {
       <Router>
         <NavItems/>
         <div className="action-panel-content">
-          <Route path="/app/filter" exact component={Filter} />
-          <Route path="/app/edit" exact component={Edit} />
-          <Route path="/app/visualise" exact component={Visualise} />
-          <Route path="/app/export" exact component={Export} />
-          <Route path="/app" exact render={() => (<Redirect to="/app/filter"></Redirect>)}/>
+          <Switch>
+            <Route path="/app/filter" exact component={Filter} />
+            <Route path="/app/edit" exact component={Edit} />
+            <Route path="/app/visualise" exact component={Visualise} />
+            <Route path="/app/export" exact component={Export} />
+            <Route path="/app" render={() => (<Redirect to="/app/filter"></Redirect>)}/>
+          </Switch>
         </div>
       </Router>
     )
