7c8c43cae1c91ac170f0df690c78a35e0740ea52
Chris Martin <chris@crunchycloud.io>
2019-03-29 17:15:17 +1000

    update body content, focus csv viewer csv editor and csv file query keywords

--- src/visualise/containers/Visualise.js
+++ src/visualise/containers/Visualise.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Visualise extends Component {
   render() {
     return (
-      <div>Pretty Charting Goes Here</div>
+      <div>Coming Soon</div>
     )
   }
 }
