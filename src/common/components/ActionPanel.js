ca04f9480bc7b77b31a85458b8e6d9ad3d98f0a0
Chris Martin <chris@crunchycloud.io>
2019-03-30 22:27:50 +1000

    add lazy loading to substates

--- src/common/components/ActionPanel.js
+++ src/common/components/ActionPanel.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense } from 'react'
 import '../styles/ActionPanel.css';
 
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { Loader } from 'semantic-ui-react';
 
 import NavItems from './NavItems';
-import Filter from '../../filter/containers/Filter';
-import Edit from '../../edit/containers/Edit';
-import Visualise from '../../visualise/containers/Visualise';
-import Export from '../../export/containers/Export';
+
+const Filter = React.lazy(() => import('../../filter/containers/Filter'));
+const Edit = React.lazy(() => import('../../edit/containers/Edit'));
+const Visualise = React.lazy(() => import('../../visualise/containers/Visualise'));
+const Export = React.lazy(() => import('../../export/containers/Export'));
 
 class ActionPanel extends Component {
   render() {
@@ -16,10 +18,26 @@ class ActionPanel extends Component {
         <NavItems/>
         <div className="action-panel-content">
           <Switch>
-            <Route path="/app/filter" exact component={Filter} />
-            <Route path="/app/edit" exact component={Edit} />
-            <Route path="/app/visualise" exact component={Visualise} />
-            <Route path="/app/export" exact component={Export} />
+            <Route path="/app/filter" exact render={() => (
+              <Suspense fallback={<Loader active inline="centered"/>}>
+                <Filter/>
+              </Suspense>
+            )} />
+            <Route path="/app/edit" exact render={() => (
+              <Suspense fallback={<Loader active inline="centered"/>}>
+                <Edit/>
+              </Suspense>          
+            )} />
+            <Route path="/app/visualise" exact render={() => (
+              <Suspense fallback={<Loader active inline="centered"/>}>
+                <Visualise/>
+              </Suspense>
+            )} />
+            <Route path="/app/export" exact render={() => (
+              <Suspense fallback={<Loader active inline="centered"/>}>
+                <Export/>
+              </Suspense>
+            )} />
             <Route path="/app" render={() => (<Redirect to="/app/filter"></Redirect>)}/>
           </Switch>
         </div>
